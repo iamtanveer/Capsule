3eb662b1575a928ddabbae3630bbc2fa82ef765c
Tanveer Singh <tanveer7537@gmail.com>
2017-06-07 20:25:26 +0530

    Added two protocols and UIPickerView is now fully customized

--- Capsule/ViewController.swift
+++ Capsule/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet var statePicker: UIPickerView!
+    @IBOutlet var statePickerBtn: UIButton!
+    
+    let states = ["Delhi", "West Bengal", "Assam", "Punjab", "Haryana", "Jharkhand"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1  //number of columns = 1 column ie "state"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count  //how many states do you wanna put
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal) //for changing of button text
+        statePicker.isHidden = true
+    }
 }
 
